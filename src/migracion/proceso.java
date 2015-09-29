2118b04cdd8d3baa8b17359892d4dd84e9aef370
fernandosanchez1128 <fernandosanchez1128@gmail.com>
2015-09-28 22:02:02 -0500

    integracion de varias tablas

--- src/migracion/proceso.java
+++ src/migracion/proceso.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 
 /**
@@ -31,38 +32,39 @@ public class proceso {
          Statement stmt = conn.createStatement();
          
       
-         
-     ResultSet rs = stmt.executeQuery("SELECT * FROM dbo.dimScenario");
-     Connection con_postgres = conexion.conectarPostgres();
-     Statement stmt_post = con_postgres.createStatement();
-     String sql_save ="";
-         
-     while (rs.next())
+     String [] tablas = {"dbo.dimCurrency","dim_currency","dbo.dimDate","dim_date"};
+     
+     for (int index=1; index <= tablas.length;index+=2)
      {
-         ResultSetMetaData rsMd = rs.getMetaData();
-         int cantidadColumnas = rsMd.getColumnCount();
-         sql_save = "INSERT INTO dimscenario values (";
-         for (int i=1;i<=cantidadColumnas;i++)
-         {
-                if (i!=cantidadColumnas)
-                {
-                    sql_save += "'"+rs.getObject(i)+"'"+",";
-                }
-                else
-                {
-                    sql_save += "'" +rs.getObject(i)+"'";
-                }
-                
-         }
-         sql_save +=");";
-         System.out.println(sql_save);
-         stmt_post.executeUpdate(sql_save);
-     }
-         
      
-     
-     
-         
+        String sql_consulta =  "SELECT * FROM " + tablas[index-1];
+        ResultSet rs = stmt.executeQuery(sql_consulta);
+        Connection con_postgres = conexion.conectarPostgres();
+        Statement stmt_post = con_postgres.createStatement();
+        String sql_save ="";
+
+        while (rs.next())
+        {
+            ResultSetMetaData rsMd = rs.getMetaData();
+            int cantidadColumnas = rsMd.getColumnCount();
+            sql_save = "INSERT INTO " + tablas[index] + " values (";
+            for (int i=1;i<=cantidadColumnas;i++)
+            {
+                   if (i!=cantidadColumnas)
+                   {
+                       sql_save += "'"+rs.getObject(i)+"'"+",";
+                   }
+                   else
+                   {
+                       sql_save += "'" +rs.getObject(i)+"'";
+                   }
+
+            }
+            sql_save +=");";
+            System.out.println(sql_save);
+            stmt_post.executeUpdate(sql_save);
+           }
+     }       
      }
      
      
